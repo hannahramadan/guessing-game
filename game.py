393982990c59af242edfdfd0246f7c348c242904
Hannah Ramadan <hannahr315@gmail.com>
2021-01-16 20:52:02 +0000

    Added randomized and check if guessed number in range

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 """A number-guessing game."""
-
+import random 
 """
 greet player
 get player name
@@ -19,15 +19,19 @@ to-do:
 """
 print("Hello, welcome to the guessing game!")
 player_name = input("What is your name? ")
-rand_num = 32
+rand_num = random.randint(1,100)
 str_num = input("Please choose a number between 1 and 100: ")
 num = int(str_num)
+while (num >100 or num < 1):
+    str_num = input("Are you high? I said between 1 and 100, please. ")
+    num = int(str_num)
+
 counter = 1
 while (num != rand_num):
-    if (num < 32):
-        print("Your guess is too low")
-    elif (num > 32):
-        print("You guess is too high")
+    if (num < rand_num):
+        print("Your guess is too low.")
+    elif (num > rand_num):
+        print("Your guess is too high.")
     str_num = input("Please guess another number between 1 and 100: ")
     num = int(str_num) 
     counter += 1
